d87a90a318646bdf81efa1a71392f300f45f8d5b
Lukas Fajt <fajtak.l@gmail.com>
2021-08-12 10:03:26 +0200

    more info when a single city is searched

--- main.py
+++ main.py
@@ -39,20 +39,7 @@ class KeywordQueryEventListener(EventListener):
 										description='Pressure: %s Pa, Humidity: %s%%, Wind: %s m/s, Cloudiness: %s%%' % (press,hum,wind,cloud),
 										on_enter=OpenUrlAction(gen_url(cityID))))
 
-	def add_future_precipitations(self, items, city):
-		r = requests.get("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + self.apikey + "&units=metric")
-		data_string = r.json()
-
-		lon = data_string["coord"]["lon"]
-		lat = data_string["coord"]["lat"]
-		icon = data_string["weather"][0]["icon"]
-		cityID = data_string["id"]
-
-		r = requests.get("https://api.openweathermap.org/data/2.5/onecall?lat=" + str(lat) + "&lon=" + str(lon) + '&exclude=hourly,daily,alerts&appid=' + self.apikey + "&units=metric")
-		data_string = r.json()
-		# recent_time = data_string["current"]["dt"]
-		precip = data_string["minutely"]
-
+	def precip_in_hour(self,items,precip,icon,cityID):
 		total_prec = 0.0
 		for event in precip:
 			total_prec += event["precipitation"]
@@ -87,7 +74,86 @@ class KeywordQueryEventListener(EventListener):
 			items.append(ExtensionResultItem(icon='images/'+icon[0:2]+'d.png',
 										name='It should start raining in %s minutes' % (rainStartTime),
 										description='Expected precipitations in the next hour: %2.1f mm/h' % (total_prec/len(precip)),
-										on_enter=HideWindowAction()))
+										on_enter=OpenUrlAction(gen_url(cityID))))
+
+	def precip_in_12hours(self,items,hourly,icon,cityID):
+		# dt = datetime.datetime.now()
+		total_prec = 0.0
+		times = ""
+		precip = ""
+		nEntries = 0
+		for eventID in range(12):
+			if nEntries == 6:
+				times += " ..."
+				break
+			if "rain" in hourly[eventID]:
+				precip += "" + str(hourly[eventID]["rain"]["1h"]) + "mm/h   "
+				nEntries += 1
+			else:
+				continue
+				# precip += "  " + "0 mm/h" + "  "
+			time = datetime.datetime.fromtimestamp(hourly[eventID]["dt"])
+			times += " " + str(time.hour)
+			if time.hour < 12:
+				times += "am    "
+			else:
+				times += "pm    "
+		if (len(precip) == 0):
+			items.append(ExtensionResultItem(icon='images/'+icon[0:2]+'d.png',
+										name='No rain in the next 12 hours!',
+										description='',
+										on_enter=OpenUrlAction(gen_url(cityID))))
+		else:
+			items.append(ExtensionResultItem(icon='images/'+icon[0:2]+'d.png',
+										name=str(times),
+										description=str(precip),
+										on_enter=OpenUrlAction(gen_url(cityID))))
+
+	def add_future_precipitations(self, items, city):
+		r = requests.get("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + self.apikey + "&units=metric")
+		data_string = r.json()
+
+		lon = data_string["coord"]["lon"]
+		lat = data_string["coord"]["lat"]
+		icon = data_string["weather"][0]["icon"]
+		cityID = data_string["id"]
+
+		r = requests.get("https://api.openweathermap.org/data/2.5/onecall?lat=" + str(lat) + "&lon=" + str(lon) + '&exclude=current,daily,alerts&appid=' + self.apikey + "&units=metric")
+		data_string = r.json()
+		# recent_time = data_string["current"]["dt"]
+		precip = data_string["minutely"]
+		self.precip_in_hour(items,precip,icon,cityID)
+		hourly = data_string["hourly"]
+		self.precip_in_12hours(items,hourly,icon,cityID)
+
+	def add_3day_forecast(self, items, city):
+		r = requests.get("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + self.apikey + "&units=metric")
+		data_string = r.json()
+
+		lon = data_string["coord"]["lon"]
+		lat = data_string["coord"]["lat"]
+		icon = data_string["weather"][0]["icon"]
+		cityID = data_string["id"]
+
+		r = requests.get("https://api.openweathermap.org/data/2.5/onecall?lat=" + str(lat) + "&lon=" + str(lon) + '&exclude=current,minutely,hourly,alerts&appid=' + self.apikey + "&units=metric")
+		data_string = r.json()
+		# recent_time = data_string["current"]["dt"]
+		daily = data_string["daily"]
+
+		for day in range(1,4):
+			time = datetime.datetime.fromtimestamp(daily[day]["dt"])
+			descr = ""
+			if "rain" in daily[day]:
+				items.append(ExtensionResultItem(icon='images/'+daily[day]["weather"][0]["icon"][0:2]+'d.png',
+										name='%s, %s %s - %s:' % (str(time.strftime("%a")),str(time.strftime("%d")),str(time.strftime("%b")),daily[day]["weather"][0]["description"]),
+										description='%2.0f / %2.0f %sC, %2.1f mm/day' % (daily[day]["temp"]["max"],daily[day]["temp"]["min"],chr(176),daily[day]["rain"]),
+										on_enter=OpenUrlAction(gen_url(cityID))))
+			else:
+				items.append(ExtensionResultItem(icon='images/'+daily[day]["weather"][0]["icon"][0:2]+'d.png',
+										name='%s, %s %s - %s' % (str(time.strftime("%a")),str(time.strftime("%d")),str(time.strftime("%b")),daily[day]["weather"][0]["description"]),
+										description='%2.0f / %2.0f %sC' % (daily[day]["temp"]["max"],daily[day]["temp"]["min"],chr(176)),
+										on_enter=OpenUrlAction(gen_url(cityID))))
+
 
 	def on_event(self, event, extension):
 		items = []
@@ -99,6 +165,7 @@ class KeywordQueryEventListener(EventListener):
 		if (city != None):
 			self.add_current_weather(items,city)
 			self.add_future_precipitations(items,city)
+			self.add_3day_forecast(items,city)
 		else:
 			for iterCity in predef_cities:
 				self.add_current_weather(items,iterCity)
